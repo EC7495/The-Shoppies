74c8a1060bfddf8b6e6d7d219b7da9f1795afdf2
Erick <ecanals07@gmail.com>
2020-08-26 01:13:09 -0400

    added user authentication to allow users to log in and have their nominations persist

---
+++ server/auth/index.js
@@ -0,0 +1,45 @@
+const router = require('express').Router()
+const Users = require('../db/models/Users')
+
+router.get('/me', (req, res) => {
+  res.json(req.user)
+})
+
+router.post('/login', async (req, res, next) => {
+  try {
+    const user = await Users.findOne({
+      where: { username: req.body.username },
+    })
+    if (!user || !user.correctPassword(req.body.password)) {
+      res.status(401).json({ error: 'Wrong username and/or password' })
+    } else {
+      req.login(user, error => (error ? next(error) : res.json(user)))
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/signup', async (req, res, next) => {
+  try {
+    const user = await Users.create({
+      username: req.body.username,
+      password: req.body.password,
+    })
+    req.login(user, error => (error ? next(error) : res.json(user)))
+  } catch (error) {
+    if (error.name === 'SequelizeUniqueConstraintError') {
+      res.status(401).send({ error: 'User already exists' })
+    } else {
+      next(error)
+    }
+  }
+})
+
+router.post('/logout', (req, res) => {
+  req.logout()
+  req.session.destroy()
+  res.redirect('/')
+})
+
+module.exports = router
