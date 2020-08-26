d68294471f5e5583ee51a6c69230d73a396b369b
Erick <ecanals07@gmail.com>
2020-08-26 15:25:02 -0400

    added a form for users to search for movies by title, year, and type

--- client/components/Movies.jsx
+++ client/components/Movies.jsx
@@ -1,34 +1,104 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import {
+  TextField,
+  InputLabel,
+  Select,
+  MenuItem,
+  Button,
+  makeStyles,
+} from '@material-ui/core'
+
+const useStyles = makeStyles(theme => ({
+  movies: {
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+
+  form: {
+    width: '35%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+}))
 
 const Movies = () => {
+  const classes = useStyles()
   const [movieTitle, setMovieTitle] = useState('')
+  const [movieYear, setMovieYear] = useState('')
+  const [movieType, setMovieType] = useState('')
   const [movies, setMovies] = useState([])
 
   const handleOnChange = event => {
-    setMovieTitle(event.target.value)
+    switch (event.target.name) {
+      case 'title':
+        setMovieTitle(event.target.value)
+        break
+      case 'year':
+        setMovieYear(event.target.value)
+        break
+      case 'type':
+        setMovieType(event.target.value)
+        break
+      default:
+        return
+    }
   }
 
   const handleOnSubmit = async event => {
     event.preventDefault()
     setMovieTitle('')
+    setMovieYear('')
+    setMovieType('')
     const {
       data: { Search: fetchedMovies },
-    } = await axios.get(`/api/movies/${movieTitle}`)
+    } = await axios.get(
+      `/api/movies/search-movies/?title=${movieTitle}&year=${movieYear}&type=${movieType}`
+    )
 
     setMovies(fetchedMovies)
   }
 
   return (
-    <div id="movies">
-      <form onSubmit={handleOnSubmit}>
-        <label htmlFor="movie-title">Title</label>
-        <input
-          name="movie-title"
+    <div id="movies" className={classes.movies}>
+      <form onSubmit={handleOnSubmit} className={classes.form}>
+        <TextField
+          required
+          name="title"
+          label="Title"
+          variant="outlined"
           value={movieTitle}
           onChange={handleOnChange}
         />
-        <button type="submit">Enter</button>
+        <TextField
+          name="year"
+          label="Year"
+          variant="outlined"
+          value={movieYear}
+          onChange={handleOnChange}
+          type="number"
+          min={1900}
+          max={new Date().getFullYear()}
+        />
+        <div id="movie-type">
+          <InputLabel>Type</InputLabel>
+          <Select
+            name="type"
+            variant="outlined"
+            value={movieType}
+            onChange={handleOnChange}
+          >
+            <MenuItem value="movie">Movie</MenuItem>
+            <MenuItem value="series">Series</MenuItem>
+            <MenuItem value="episode">Episode</MenuItem>
+          </Select>
+        </div>
+        <Button type="submit" variant="outlined">
+          Enter
+        </Button>
         {movies.map(movie => (
           <div key={movie.imdbID}>
             <h1>{movie.Title}</h1>
--- client/index.jsx
+++ client/index.jsx
@@ -3,7 +3,7 @@ import { render } from 'react-dom'
 import { Movies } from './components'
 
 const App = () => (
-  <div>
+  <div id="index">
     <h1>Welcome to my puppies page</h1>
     <Movies />
   </div>
